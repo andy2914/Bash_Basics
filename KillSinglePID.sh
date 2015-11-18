2abafdd0002c20af777e1bcf8188a5ad0b86c2a9
andy2914 <andrew.abraham87@gmail.com>
2015-11-18 13:32:50 -0500

    Create KillSinglePID.sh

---
+++ KillSinglePID.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pid=$(ps axo pid,command | grep "[A]crobat" | awk '{print $1}')
+
+echo "Pid is: "$pid
+
+if [ "$pid" ]
+    then
+        echo "Acrobat is Running"
+        echo "Killing Acrobat pid"
+            kill $pid
+        echo "Pid killed"
+    else
+        echo "Acrobat not running"
+    fi
