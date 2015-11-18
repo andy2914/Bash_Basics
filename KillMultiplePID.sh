00bcc37a34a44496ccee04279013147ce2ef38cb
andy2914 <andrew.abraham87@gmail.com>
2015-11-18 13:34:46 -0500

    Kill Multiple PIDs

---
+++ KillMultiplePID.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+procList=("Microsoft Word"
+"Safari"
+"Firefox"
+"Preview")
+
+for proc in "${procList[@]}"; do
+    runningProc=$(ps axc | grep -i "$proc" | awk '{print $1}')
+    if [[ $runningProc ]]; then
+        echo "Found running process $proc with PID: ${runningProc}. Killing it..."
+        kill $runningProc
+    else
+        echo "$proc not found running..."
+    fi
+done
