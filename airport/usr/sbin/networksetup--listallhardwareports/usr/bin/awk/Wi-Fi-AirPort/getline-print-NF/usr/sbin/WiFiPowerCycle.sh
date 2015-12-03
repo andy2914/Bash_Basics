11444da6b42d1baa9de1ebef954d29022f3393bf
andy2914 <andrew.abraham87@gmail.com>
2015-12-03 11:42:25 -0500

    Delete WiFiPowerCycle.sh

--- airport/usr/sbin/networksetup--listallhardwareports/usr/bin/awk/Wi-Fi-AirPort/getline-print-NF/usr/sbin/WiFiPowerCycle.sh
+++
@@ -1,3 +0,0 @@
-airport=`/usr/sbin/networksetup -listallhardwareports | /usr/bin/awk '/Wi-Fi|AirPort/ {getline; print $NF}'`
-    /usr/sbin/networksetup -setairportpower $airport off
-    echo "power to $airport is off."
